2d4287c0caea8fe27c00383f4da38b12118bf9c7
catcrumpet <cikoykip@gmail.com>
2020-04-06 18:47:01 -0700

    fixed summarise_day

--- R/summarise_day.R
+++ R/summarise_day.R
@@ -8,6 +8,7 @@ summarise_day <- function(acc_data, counts = axis1, pa = pa, valid = !nonwear) {
                     !!enquo(pa),
                     !!enquo(valid),
                     data_table = TRUE) %>%
+    lazy_dt(key_by = timestamp) %>%
     mutate(date = lubridate::date(timestamp)) %>%
     group_by(date) %>%
     group_modify(summarise_chunk_, epochlength = epochlength)
