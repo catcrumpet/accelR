0d0b032e2c563011df0d08486d9bfe1ab69ff253
catcrumpet <cikoykip@gmail.com>
2020-05-01 12:20:24 -0700

    removed data.table conversion

--- R/summarise_day.R
+++ R/summarise_day.R
@@ -6,11 +6,11 @@ summarise_day <- function(acc_data, counts = axis1, pa = pa, valid = !nonwear) {
   standardize_data_(acc_data,
                     !!enquo(counts),
                     !!enquo(pa),
-                    !!enquo(valid),
-                    data_table = TRUE) %>%
-    lazy_dt(key_by = timestamp) %>%
+                    !!enquo(valid)) %>%
+    #                 data_table = TRUE) %>%
+    # lazy_dt(key_by = timestamp) %>%
     mutate(date = lubridate::date(timestamp)) %>%
     group_by(date) %>%
-    group_modify(summarise_chunk_, epochlength = epochlength) %>%
-    as_tibble()
+    group_modify(~summarise_chunk_(., epochlength = epochlength))
+    # as_tibble()
 }
